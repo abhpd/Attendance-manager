ec79dec03588e1e420f77e44a29792dc8744090e
Abhishek Prasad <abhpdmail@gmail.com>
2020-01-25 23:44:20 +0530

    Initial commit

---
+++ .vscode/displayscreen.cpp
@@ -0,0 +1,7 @@
+void display_menu(){
+    cout<<"------------------"<<endl;    
+    cout<<"ATTENDANCE-MANAGER"<<endl;
+    cout<<"------------------"<<endl<<endl;
+
+    cout<<"1.log\n2.View Status\n3.Edit Subjects";
+}
---
+++ .vscode/main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include "displayscreen.cpp"
+
+using namespace std;
+
+class subject{
+    string code;
+    string name;
+    int lecture_count;
+    int present_count;
+    int absent_count;
+    int leave_count;
+public:
+    subject(string c,string n){
+        code=c;
+        name=n;
+    }
+    string return_code(){return code;}
+    void present(){lecture_count++;present_count++;}
+    void absent(){lecture_count++;absent_count++;}
+    void leave(){lecture_count++;leave_count++;}
+};
+
+class student{
+    vector<subject> subj;
+public:
+    void add_subj(string code,string name){
+        subject s(code,name);
+        subj.push_back(s);
+    }
+    void remove_subj_by_code(string code){
+        for(int i=0;i<subj.size();i++){
+            if(subj[i].return_code==code){
+                subj.erase(subj.begin()+i);
+            }
+        }
+    }
+    void log(){
+        string query_code;
+        cout<<"Enter subject code or select from the list displayed followed by X(X=P/A/L)>";
+    }
+};
+
+int main()
+{
+    while(true){
+        display_menu();
+        return 0;
+    }
+    return 0;
+}
