feaa776badb7c4663abe4dd67e1e887606d9c870
Abhishek Prasad <abhpdmail@gmail.com>
2020-01-25 23:57:06 +0530

    seperatingclassanddisplayscreens

--- displayscreen.cpp
+++ displayscreen.cpp
@@ -1,7 +1,18 @@
+using namespace std;
 void display_menu(){
     cout<<"------------------"<<endl;    
     cout<<"ATTENDANCE-MANAGER"<<endl;
     cout<<"------------------"<<endl<<endl;
 
-    cout<<"1.log\n2.View Status\n3.Edit Subjects";
+    cout<<"1.log\n2.View Status\n3.Edit Subjects\n\n:>";
+}
+void setupmenu(){
+    cout<<"------------------"<<endl;    
+    cout<<"ATTENDANCE-MANAGER"<<endl;
+    cout<<"------------------"<<endl<<endl;
+
+    cout<<"1.Edit Subjects\n\n:>";
+}
+void logmenu(){
+    
 }
